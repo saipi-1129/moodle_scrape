f487f3fa3dad30f7b2fb59c38ac8b76c69d282d9
さいぴ <funnyorange.gg@gmail.com>
2024-10-16 21:18:04 +0900

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,3 +1,5 @@
 class config:
+    login_url = "https://moodle2024.mc2.osakac.ac.jp/2024/login/index.php"
+    dashboard_url = "https://moodle2024.mc2.osakac.ac.jp/2024/calendar/view.php?view=day&time={current_timestamp}"
     NAME = ""
     PASS = ""
